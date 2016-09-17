8c7dcd6d99628b2e512ade05470daab16d6df276
Bruno Moreno <brunomorello7@gmail.com>
2016-09-17 11:51:05 -0300

    Finish static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+## Just to show errors in red
+require 'minitest/reporters'
+Minitest::Reporters.use!
+## end of Minitest Reporters
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
