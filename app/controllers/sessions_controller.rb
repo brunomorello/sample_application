282f8b9dd0f631483ed1353d4977a22880d514b3
Bruno Moreno <brunomorello7@gmail.com>
2016-11-08 22:18:15 -0200

    Implementing Remember User Logged with encrypted cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,17 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		# Login the user in and redirect to the user's show page
       log_in user
+      remember user
       redirect_to user
   	else
   		# Create an error message
-  		flash.now[:warning] = 'Invalid email/password combination'
+  		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'  	
   	end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,18 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	# Remembers a user in a persistent session
+	def remember(user)
+		user.remember
+		#cookies[:user_id] = { value: user.id, expires: 20.years.from_now.utc }
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
 	# Returns the current logged-in user(if any)
 	def current_user
 		# Following the Ruby Convetion - Uses Memoization
-		@current_user ||= User.find_by(id: session[:user_id])
+		# @current_user ||= User.find_by(id: session[:user_id])
 
 		# The line above is the same:
 		# @current_user = @current_user || User.find_by(id: session[:user_id])
@@ -17,6 +25,16 @@ module SessionsHelper
 		# else 
 		# 	@current_user
 		# end
+
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end				
 	end
 
 	# Returns true if the user is logged-in
@@ -24,5 +42,9 @@ module SessionsHelper
 		!current_user.nil?
 	end	
 
-	
+	# Logs out the current user
+	def log_out	
+		session.delete(:user_id)
+		@current_user = nil
+	end
 end
