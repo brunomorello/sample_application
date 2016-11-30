44f402118c02aefae7c4bbed109e20918f634ffa
Bruno Moreno <brunomorello7@gmail.com>
2016-11-29 22:39:48 -0200

    Improvement - Showing All Users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,12 @@
 module UsersHelper
 
 	# Returns the Gravatar for the given user
-	def gravatar_for(user)
+	def gravatar_for(user, options = { size: 80 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
+
 		#gravatar_url = "https://secure.gravatar.com/#{gravatar_id}" - It don't work
-		gravatar_url = "http://2.gravatar.com/avatar/#{gravatar_id}"
+		gravatar_url = "http://2.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
 end
