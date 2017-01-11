90cf5853fd5a7a7316477234ae218374b6a1768d
Bruno Moreno <brunomorello7@gmail.com>
2017-01-10 22:14:36 -0200

    New Feature - Micropost with images

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,20 @@ class Micropost < ActiveRecord::Base
   # Use Staby Lambda
   default_scope -> { order(created_at: :desc) }
 
+  # Uploader
+  mount_uploader :picture, PictureUploader
+
   # Validations
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+    # Validates the size of an uploaded picture
+    def picture_size
+        if picture.size > 5.megabytes
+            errors.add(:picture, "should be less than 5MB")
+        end
+    end
 end
---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,11 @@
+#if Rails.env.production?
+#    CarrierWave.configure do |config|
+#        config.flag_credentials = {
+            # Configuration for Amazon S3
+#            :provider              => 'AWS',
+#            :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+#            :aws_secret_access_key => ENV['S3_SECRET_KEY']
+#        }
+#        config.flag_directory = ENV['S3_BUCKET']
+#    end
+#end
